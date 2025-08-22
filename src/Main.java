7792565c6ce8b72580d66af1ce346d103ed88296
user <tangirovzafar203@gmail.com>
2025-08-22 17:17:55 +0500

    two_pointers commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,2,2,3,3,3,3};
+        int k = removeDuplicates(arr);
+
+        int[] result = Arrays.copyOfRange(arr, 0, k);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int removeDuplicates(int[] arr) {
+        if (arr.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] != arr[i]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+}
